dbfd9c55997b6b7bb12bc95efab83395bf2ae25a
ZeroRBK <guilherme.andrade13579@gmail.com>
2025-07-26 12:04:34 -0500

    Refactored geolocation logic into LocationCard; made Home.jsx server component

---
+++ src/components/LocationCard.jsx
@@ -0,0 +1,25 @@
+
+"use client";
+
+import Button from "./Button";
+import Card from "./Card";
+import { useUserLocation } from "../hooks/useUserLocation";
+
+export default function LocationCard() {
+  const { location, error, requestLocation } = useUserLocation();
+
+  return (
+    <>
+      <Button onClick={requestLocation}>Show me my local solar potential!</Button>
+
+      {error && <p className="text-red-500">{error}</p>}
+
+      {location && (
+        <Card title="Your Location">
+          <p>Latitude: {location.latitude.toFixed(4)}</p>
+          <p>Longitude: {location.longitude.toFixed(4)}</p>
+        </Card>
+      )}
+    </>
+  );
+}
